1519d33f99b329fd209e8d7c5f11c4b4dc8fa427
B-arney <barnabas.benko0427@gmail.com>
2025-04-09 16:06:10 +0200

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
-import "../global.css";
 
 export default function Index() {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}>
-            <Text>Hello World!</Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
 }
